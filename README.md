e67006dc67f445ccaaed19e7dc119efc31827aef
kuzhankuixiong <1733884649@qq.com>
2019-05-23 02:11:35 +0100

    files used to create testPypi package added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FYP_neural_net_doc
+# Brian2docs
+
+This package create pdf document for brian2 network.
+You can document you neural network by put everything you want to document into a network project,
+and use the corresponding function in this package.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="brian2docs",
+    version="0.2.0",
+    author="Zeqian Cao",
+    author_email="1733884649@qq.com",
+    description="A package generate pdf document for brian2 neural network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kuzhankuixiong/FYP_neural_net_doc",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
