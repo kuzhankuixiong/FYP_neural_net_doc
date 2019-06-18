fcc8f22bd09253ce8a3839aa15dfb402044972d1
kuzhankuixiong <1733884649@qq.com>
2019-06-18 19:19:49 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="brian2docs",
-    version="0.2.0",
+    version="1.0.0",
     author="Zeqian Cao",
     author_email="1733884649@qq.com",
     description="A package generate pdf document for brian2 neural network",
